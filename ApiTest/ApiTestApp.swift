5e4ea5ad2c3c690d8a7d4255c7e3b4f54d42980e
Matthew Collins <m.collins@etoncollege.org.uk>
2023-03-17 20:16:16 +0000

    Initial Commit

---
+++ ApiTest/ApiTestApp.swift
@@ -0,0 +1,17 @@
+//
+//  ApiTestApp.swift
+//  ApiTest
+//
+//  Created by Collins, Matthew - MC on 17/03/2023.
+//
+
+import SwiftUI
+
+@main
+struct ApiTestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
