15e0c1f10ba7166e8eb7597fb17eb52b61430400
Matthew Collins <m.collins@etoncollege.org.uk>
2023-03-17 20:30:15 +0000

    retrieve data from a url session

--- ApiTest/ApiManager.swift
+++ ApiTest/ApiManager.swift
@@ -10,9 +10,23 @@ import Foundation
 class ApiManager {
     
     func getData() async {
+        
+        // INSTANTIATE AND HANDLE A NEW URL OBJECT
         guard let url = URL(string: "https://tradestie.com/api/v1/apps/reddit") else {
             print("Invalid URL")
             return
+        }
+        
+        do {
+            // START A URL SESSION TO GET DATA
+            let (data, _) = try await URLSession.shared.data(from: url)
+            print(data)
+            
+            // DECODE THE DATA TO BE ABLE TO MAKE SENSE OF IT IN JSON
+            
+        }
+        catch {
+            print("Invalid data")
         }
     }
 }
